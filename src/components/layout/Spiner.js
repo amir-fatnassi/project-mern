bfa5b3084f9c9a2c78b9869d4d81293dd3c7a1cd
amir <fatnasiamir@yahoo.fr>
2019-10-06 23:12:42 +0200

    projectmern

---
+++ src/components/layout/Spiner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Spinner from './Spinner.gif'
+
+const Spiner = () =><>
+            <img src={Spinner} 
+            alt="loading..." 
+            style={{ width: '200px', margin: 'auto', display: 'block' }} />
+        </>
+export default Spiner
+ 
---
+++ src/components/pages/home.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+import Users from '../users/Users'
+import Search from '../users/Search'
+
+const home = () => <Fragment>
+                        <Search /> 
+                        <Users/>
+                    </Fragment>
+
+export default home
